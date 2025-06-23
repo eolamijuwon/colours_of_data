f5e23956284747a9d3426c0c695785d166a4011f
Emmanuel Olamijuwon <emmanuel@olamijuwon.com>
2025-06-23 20:20:25 +0200

    updates 23June

--- 01-prerequisites/install-pkg.R
+++ 01-prerequisites/install-pkg.R
@@ -1,4 +1,10 @@
 ## Install the following R-packages
 
-install.packages("dplyr")
-install.packages("ggplot2")
+
+install.packages ("dplyr")      ## Tidy data cleaning
+install.packages ("ggplot2")    ## Elegant graphics
+install.packages ("readr")      ## Reading tabular data
+install.packages ("tidyr")      ## Text rendering
+install.packages ("showtext")   ## Text rendering
+install.packages ("extrafont")  ## Loading fonts
+install.packages ("ggtext")     ## Improved text rendering
